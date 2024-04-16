88ffde80688606f25298279280798cc757be6d67
Coden Stark <codenstark@gmail.com>
2024-04-16 11:13:06 -0700

    StratLayer table made

--- tables.sql
+++ tables.sql
@@ -29,3 +29,12 @@
 
 
 -- StratLayer table:
+CREATE TABLE StratLayer (
+    OutcropID INT UNSIGNED,
+    LayerNumber INT UNSIGNED,
+    Description varchar(100) NOT NULL,
+    TopDepth INT NOT NULL,
+    BottomDepth INT NOT NULL,
+    PRIMARY KEY (OutcropID, LayerNumber),
+    FOREIGN KEY (OutcropID) REFERENCES Outcrop(ID), 
+);
