b9428a25447eb600473450505953c4e1bb352bca
Coden Stark <codenstark@gmail.com>
2024-04-16 11:19:49 -0700

    started Sample table

--- tables.sql
+++ tables.sql
@@ -4,9 +4,20 @@
 
 
 -- Sample table:
-
-
-
+CREATE TABLE Sample (
+    ID INT UNSIGNED PRIMARY KEY,
+    OutcropID INT UNSIGNED NOT NULL,
+    ResearcherID INT UNSIGNED NOT NULL,
+    Type VARCHAR(16) NOT NULL,
+    Depth INT NOT NULL,
+    Description VARCHAR(500) NOT NULL,
+    Color VARCHAR(10),
+    Date DATETIME NOT NULL,
+    CoarseFraction DECIMAL(5,4),
+    FineFraction DECIMAL(5,4),
+    LayerNumber INT UNSIGNED,
+    -- FOREIGN KEYS
+);
 
 -- Photo table:
 
@@ -32,9 +43,9 @@
 CREATE TABLE StratLayer (
     OutcropID INT UNSIGNED,
     LayerNumber INT UNSIGNED,
-    Description varchar(100) NOT NULL,
+    Description VARCHAR(100) NOT NULL,
     TopDepth INT NOT NULL,
     BottomDepth INT NOT NULL,
     PRIMARY KEY (OutcropID, LayerNumber),
-    FOREIGN KEY (OutcropID) REFERENCES Outcrop(ID), 
+    FOREIGN KEY (OutcropID) REFERENCES Outcrop(ID) 
 );
