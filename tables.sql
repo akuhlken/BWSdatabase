067b22634480715b25cc48717efe7e4c136268e4
Striker1046 <98913744+Striker1046@users.noreply.github.com>
2024-04-16 11:26:40 -0700

    outcrop table done

--- tables.sql
+++ tables.sql
@@ -24,7 +24,18 @@
 
 
 -- Outcrop table:
-
+CREATE TABLE Outcrop (
+   ID INT UNSIGNED AUTO_INCREMENT,
+   Name varchar(64) NOT NULL UNIQUE,
+   Description VARCHAR(500) NOT NULL,
+   Location Point NOT NULL,
+   3Dmodel VARCHAR(100),
+   OrthophotoID INT UNSIGNED,
+   PRIMARY KEY (ID),
+   FOREIGN KEY (OrthophotoID) REFERENCES Photo(ID) 
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+);
 
 
 
