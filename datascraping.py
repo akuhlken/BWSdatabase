3c09fb2b253c5c50154d8221b6969fc672c287ce
Coden Stark <codenstark@gmail.com>
2024-04-22 12:43:52 -0700

    data scraping python file, takes csv and exports import statement to a text file

---
+++ datascraping.py
@@ -0,0 +1,45 @@
+# loop through all rows in a csv file
+
+# parse each row
+
+# sort relevant information into the correct insert order and write SQL insert statement
+# put insert statement into a doc.
+
+
+
+
+# input must not have column names
+
+import csv
+import os
+
+# change to the location of the file to scrape
+filename = r'C:\Users\coden\Downloads\Outcrop(Sheet1).csv'
+
+# change to where you want the data to go
+savelocation = r"C:\Users\coden\OneDrive - Whitman College\Senior\Spring 2024\results.txt"
+
+insertStatement = "INSERT INTO Outcrop (Name, Description, Location, 3Dmodel) VALUES"
+
+with open(filename, 'r') as csvfile:
+    datareader = csv.reader(csvfile)
+    for row in datareader:
+        if not "['Name', 'Description', 'Location', '3Dmodel', 'OrthophotoID']" in row:
+            insertValues = "("
+            for column in row:
+                if column.startswith("(-"):
+                    column = f"Point{column}"
+                else:
+                    column = f"\"{column}\""
+
+                if insertValues == "(":
+                    insertValues = "".join([insertValues,column])
+                else:
+                    insertValues = ",".join([insertValues,column])
+            insertValues = insertValues + ")"
+            insertStatement = ", ".join([insertStatement, insertValues])
+    insertStatement = insertStatement + ";"
+    #print(insertStatement)
+        
+with open(savelocation, "w") as save:
+    save.write(insertStatement)
