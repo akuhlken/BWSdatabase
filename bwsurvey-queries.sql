7c9245b567f453d8c1ee7893ba8c03d98ee591cb
Luca Viarengo <viarengl@whitman.edu>
2024-04-25 10:11:12 -0700

    most recent version

--- bwsurvey-queries.sql
+++ bwsurvey-queries.sql
@@ -21,7 +21,7 @@ CREATE PROCEDURE GetResearchersYear (name VARCHAR(64))
 
 --How much ____ chemical was in a basalt sample from a particular outcrop?
 
-CREATE PROCEDURE GetAllChemicalFromSample (IN chemicalName VARCHAR(20), IN outcropID VARCAHR(8))
+CREATE PROCEDURE GetAllChemicalsFromSample (IN chemicalName VARCHAR(20), IN outcropID VARCAHR(8))
     SELECT SampleChemData.chemicalName
     FROM SampleChemData
     INNER JOIN Sample ON Sample.ID = SampleChemData.SampleID
@@ -29,4 +29,10 @@ CREATE PROCEDURE GetAllChemicalFromSample (IN chemicalName VARCHAR(20), IN outcr
     WHERE Outcrop.ID = outcropID;
 
 -- What photo is associated to a certain sample?
+CREATE PROCEDURE GetPhotosFromSample (IN sampleID VARCHAR(20))
+    SELECT Photo.ImageLink
+    FROM Photo
+    INNER JOIN SamplePhoto ON SamplePhoto.ID = Photo.ID
+    INNER JOIN Sample on Outcrop.ID = Sample.OutcropID
+    WHERE Outcrop.ID = outcropID;
 -- Sample from stat layer
