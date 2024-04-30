a90b32e43e2161546aee1ceba4027af12ed66c99
kuhlkena <kuhlkena@whitman.edu>
2024-04-30 11:09:13 -0700

    added echo to test script

--- runall.sh
+++ runall.sh
@@ -1,6 +1,13 @@
 # chmod +x runall.sh
 
+echo "creatiing tables"
 mysql < bwsurvey-schema.sql
+
+echo "inserting data"
 mysql < bwsurvey-data.sql
+
+echo "creating queries"
 mysql < bwsurvey-queries.sql
+
+echo "calling queries"
 mysql < bwsurvey-examples.sql
